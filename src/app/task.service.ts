136558d99c3f28b573d9e09d41669d8378841ec6
Fábio de Oliveira Sales <fabio.oliveira.sales@gmail.com>
2019-01-15 20:12:23 -0200

    adicionando metodos de insert, update, delete e find

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { TaskModel } from './models/task.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,30 @@ export class TaskService {
 
   constructor(private db: AngularFirestore) {
     this.setTasks();
+  }
+
+
+  create(task: TaskModel): Promise<void> {
+
+    const uid = this.db.createId();
+
+    return this.tasks.doc<TaskModel>(uid).set({
+      uid: uid,
+      title: task.title,
+      done: task.done
+    });
+  }
+
+  update(task: TaskModel): Promise<void> {
+    return this.tasks.doc<TaskModel>(task.uid).update(task);
+  }
+
+  delete(task: TaskModel): Promise<void> {
+    return this.tasks.doc<TaskModel>(task.uid).delete();
+  }
+
+  get(uid: string): Observable<TaskModel> {
+    return this.tasks.doc<TaskModel>(uid).valueChanges();
   }
 
   private setTasks(): void {
